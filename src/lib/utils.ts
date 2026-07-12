6ad4b5f2c5ed91da79b280dd6c2bdcc7d3c5ff10
nihal27998 <nihalkhedekar@gmail.com>
2026-07-12 21:47:19 +0530

    Add missing cn utility helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,15 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export const formatEndTime = (hours: number) => {
-    const now = new Date();
-    const endTime = new Date(now.getTime() + hours * 60 * 60 * 1000);
-    return endTime.toLocaleString('en-US', {
-      weekday: 'short',
-      month: 'short',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    });
-  };
